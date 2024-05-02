29d2b2d7836bb19592653f678e8a92196278d16b
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2024-05-02 01:21:42 -0600

    no idea what the dot does

--- boot.asm
+++ boot.asm
@@ -1,22 +1,22 @@
-org 0x7c00
-start:
-	mov si, myString
+[org 0x7c00] ; BIOS loads boot sector to 0x7c00, so we need to tell the assembler that
 
-	mov ah, 0x0e ; Scrolling teletype BIOS routine
-	.printStr:
-	cmp byte [si], 0 ; Keep going until we find a null byte
-	je .endPrintStr
+mov si, myString
 
-	mov al, byte [si]
-	int 0x10
+mov ah, 0x0e ; Scrolling teletype BIOS routine
+printStr:
+cmp byte [si], 0 ; Keep going until we find a null byte
+je endPrintStr
 
-	inc si
-	jmp .printStr
-	.endPrintStr:
+mov al, byte [si]
+int 0x10
+
+inc si
+jmp printStr
+endPrintStr:
 
 jmp $
 
-myString db 'Hello, World!', 0
+myString: db 'Hello, World!', 0
 
-times 510-($-$$) db 0 ; Pad boot sector with zeroes to 510 bytes
-dw 0xaa55 ; Use magic number to signify that this is a boot loader to the BIOS
+times 510-($-$$) db 0	; Pad boot sector with zeroes to 510 bytes
+dw 0xaa55				; Use magic number to signify that this is a boot loader to the BIOS
