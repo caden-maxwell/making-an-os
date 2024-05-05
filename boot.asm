45e14e39bfd1e64a1a1fbf6ca020ad91bf71d1f5
cmax <a02335780@usu.edu>
2024-05-05 16:28:07 -0600

    Function to print hex values in registers

--- boot.asm
+++ boot.asm
@@ -1,22 +1,46 @@
 [org 0x7c00] ; BIOS loads boot sector to 0x7c00, so we need to tell the assembler that
 
-mov di, hello_msg
-call print_string
-
-call print_nl
-
-mov di, goodbye_msg
-call print_string
+mov di, 0x1337
+call print_hex
 
 jmp $
 
 %include "print_string.asm"
 
-hello_msg:
+print_hex:
+	push bx
+	mov bx, 3
+	.forLoop:
+		cmp bx, 0
+		jl .endForLoop
+		
+		mov cx, di
+		and cl, 0x0f
+		or cl, 0x30
+		cmp cl, 0x39 ; if cl is less than or equal to 0x39 (9)
+		jle .skipShift ; then don't skip to the alpha characters
+		add cl, 7
+		.skipShift:
+		shr di, 4 ; Shift 4 bits so we can repeat on the lowest 4 bits again
+
+		mov [bx+HEX_OUT+2], cl
+
+		dec bx
+		jmp .forLoop
+	.endForLoop:
+
+	mov di, HEX_OUT
+	call print_string
+	call print_nl
+
+	pop bx
+    ret
+
+HELLO:
 	db 'Hello, World!', 0
 
-goodbye_msg:
-	db 'Goodbye!', 0
+HEX_OUT:
+	db '0x0000', 0
 
 times 510-($-$$) db 0	; Pad boot sector with zeroes to 510 bytes
 dw 0xaa55				; Use magic number to signify that this is a boot loader to the BIOS
--- print_string.asm
+++ print_string.asm
@@ -1,6 +1,5 @@
+; Put the first address of the message into the di register
 print_string:
-    push ax
-
 	mov	ah,	0x0e ; Scrolling teletype BIOS routine
 	.forLoop:
 		cmp	byte [di], 0 ; Keep going until we find a null byte
@@ -12,17 +11,11 @@ print_string:
 		inc di
 		jmp .forLoop
 	.endForLoop:
-
-    pop ax
     ret
 
 print_nl:
-    push ax
-
     mov ax, 0x0e0a
     int 0x10
     mov ax, 0x0e0d
     int 0x10
-
-    pop ax
     ret
