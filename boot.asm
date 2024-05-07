673868a428502af96f7d44b9392322e7907a8c1c
cmax <a02335780@usu.edu>
2024-05-07 00:15:25 -0600

    Disk load and cleanup functions

--- boot.asm
+++ boot.asm
@@ -1,46 +1,35 @@
 [org 0x7c00] ; BIOS loads boot sector to 0x7c00, so we need to tell the assembler that
 
-mov di, 0x1337
+; Save the boot drive
+mov [BOOT_DRIVE], dl
+
+; Set stack pointer far away
+mov bp, 0x8000
+mov sp, bp
+
+mov bx, 0x9000 ; Read from hard disk to 0x0000(ES):0x9000(BX)
+mov dh, 2 ; Number of sectors to read (just reads dummy sectors for now)
+mov dl, [BOOT_DRIVE]
+call disk_load
+
+; Print out the first loaded word: 0x1337
+mov dx, [0x9000]
+call print_hex		
+
+; Print the first word from the 2nd loaded sector: 0xDEAD
+mov dx, [0x9000 + 512]
 call print_hex
 
 jmp $
 
 %include "print_string.asm"
+%include "print_hex.asm"
+%include "disk_load.asm"
 
-print_hex:
-	push bx
-	mov bx, 3
-	.forLoop:
-		cmp bx, 0
-		jl .endForLoop
-		
-		mov cx, di
-		and cl, 0x0f
-		or cl, 0x30
-		cmp cl, 0x39 ; if cl is less than or equal to 0x39 (9)
-		jle .skipShift ; then don't skip to the alpha characters
-		add cl, 7
-		.skipShift:
-		shr di, 4 ; Shift 4 bits so we can repeat on the lowest 4 bits again
-
-		mov [bx+HEX_OUT+2], cl
-
-		dec bx
-		jmp .forLoop
-	.endForLoop:
-
-	mov di, HEX_OUT
-	call print_string
-	call print_nl
-
-	pop bx
-    ret
-
-HELLO:
-	db 'Hello, World!', 0
-
-HEX_OUT:
-	db '0x0000', 0
+BOOT_DRIVE db 0
 
 times 510-($-$$) db 0	; Pad boot sector with zeroes to 510 bytes
 dw 0xaa55				; Use magic number to signify that this is a boot loader to the BIOS
+
+times 256 dw 0x1337
+times 256 dw 0xdead
---
+++ disk_load.asm
@@ -0,0 +1,41 @@
+; load DH sectors to ES:BX from drive DL
+mov dl, [BOOT_DRIVE]
+disk_load:
+    push dx ; Store DX on stack so later we can recall
+            ; how many sectors were requested to be read
+
+    mov ah, 0x02 ; BIOS read sector function
+    mov al, dh ; Read (DH) sectors
+    mov ch, 0x00 ; Select cylinder 0
+    mov dh, 0x00 ; Select head 0
+    mov cl, 0x02 ; Start reading from sector 2
+
+    int 0x13
+    jc disk_error ; Jump if error (carry flag is set on error)
+
+    pop dx
+    cmp dh, al ; if AL (# sectors read) != DH (# sectors expected), error out
+    jne disk_error_2
+
+    ret
+
+disk_error:
+    mov bx, DISK_ERROR_MSG
+    call print_string
+
+    mov ah, 0x01
+    int 0x13
+
+    mov dl, ah
+    mov dh, 0x00
+    call print_hex
+
+    jmp $
+
+disk_error_2:
+    mov bx, DISK_ERROR_MSG_2
+    call print_string
+    jmp $
+
+DISK_ERROR_MSG db "Disk read error: ", 0
+DISK_ERROR_MSG_2 db "Disk read error.", 0
