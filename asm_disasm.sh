0533a0531e3a8e21c97eb4bf3566467465e0bb30
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2024-05-08 04:18:57 -0600

    makefile

--- asm_disasm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gcc -ffreestanding -O0 -c basic.c -o basic.o # Compile basic C function
-# objdump -d basic.o # Show original assembly
-ld -o basic.bin -Ttext 0x0 basic.o --oformat binary
-ndisasm -b 32 basic.bin > basic.dis # Disassemble binary into basic assembly
-head -n 100 basic.dis # Show assembly from disassembled binary
