cf22cb513666315bc7309e80d6af445852675902
cmax <a02335780@usu.edu>
2024-05-08 02:51:22 -0600

    boot + kernel

--- start.sh
+++ start.sh
@@ -3,5 +3,12 @@
 # Assembles the boot file
 nasm -f bin boot/boot.asm -o boot/boot.bin
 
+# Compile and assemble kernel
+gcc -ffreestanding -c kernel/kernel.c -o kernel/kernel.o
+ld -o kernel/kernel.bin -Ttext 0x1000 kernel/kernel.o --oformat binary
+
+# Concatenate boot sector and kernel
+cat boot/boot.bin kernel/kernel.bin > os-image.bin
+
 # Starts the emulator
-qemu-system-x86_64 -drive format=raw,file=boot/boot.bin
+qemu-system-x86_64 -drive format=raw,file=os-image.bin
