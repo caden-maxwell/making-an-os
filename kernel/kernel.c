c670922021e302baa3b0d83912042ed0c1043cb6
cmax <a02335780@usu.edu>
2024-05-08 05:13:44 -0600

    red bar

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,9 +1,13 @@
 void main()
 {
-    char *video_memory = (char*) 0xb8000;
-    for (int i=0; i < 80; i++)
+    char *video_memory = (char *)0xb8000;
+    for (int i = 0; i < 80; i++)
         video_memory += 2;
-    *video_memory = 0x20;
-    video_memory++;
-    *video_memory = 0x40;
+    for (int i = 0; i < 80; i++)
+    {
+        *video_memory = 0x20;
+        video_memory++;
+        *video_memory = 0x40;
+        video_memory++;
+    }
 }
