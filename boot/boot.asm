5e3f2f03780fc6290c9c243a69d268ef5c1600e9
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2024-07-20 22:54:36 -0600

    Make boot.asm smaller, kernel.bin too large

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,3 +1,4 @@
+[bits 16]
 [org 0x7c00] ; BIOS loads boot sector to 0x7c00, so we need to tell the assembler that
 KERNEL_OFFSET equ 0x1000
 
@@ -8,23 +9,13 @@ mov [BOOT_DRIVE], dl
 mov bp, 0x9000
 mov sp, bp
 
-mov bx, MSG_RM
-call print_string
-call print_nl
-
 call load_kernel
-
 jmp switch_to_pm
 
 jmp $
 
-[bits 16]
 ; load kernel from disk
 load_kernel:
-	mov bx, MSG_KERNEL_LOAD
-	call print_string
-	call print_nl
-
 	mov bx, KERNEL_OFFSET ; Read from disk to 0x0000(ES):KERNEL_OFFSET(BX)
 	mov dh, 15 ; Number of sectors to read
 	mov dl, [BOOT_DRIVE]
@@ -42,7 +33,7 @@ begin_pm:
 	mov ebx, MSG_PM
 	call print_string_pm
 
-	call KERNEL_OFFSET
+	call KERNEL_OFFSET	; Call the kernel's entry point
 
 	jmp $
 
@@ -54,10 +45,8 @@ begin_pm:
 
 ; Global vars
 BOOT_DRIVE: db 0
-MSG_RM: db "Started boot sector in 16-bit real mode.", 0
-MSG_PM: db "Successfully booted into 32-bit protected mode.", 0
-MSG_KERNEL_LOAD: db "Loading kernel from disk...", 0
-MSG_KERNEL_SUCC: db "Successfully loaded kernel from disk.", 0
+MSG_KERNEL_SUCC: db "Loaded kernel from disk.", 0
+MSG_PM: db "Booted into 32-bit protected mode.", 0
 
 times 510-($-$$) db 0	; Pad boot sector with zeroes to 510 bytes
 dw 0xaa55				; Use magic number to signify that this is a boot loader to the BIOS
